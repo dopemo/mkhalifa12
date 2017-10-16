0a34e2df3d6d7f61447aa32a54f8b4a233b411c5
Mohamad Khalifa <mkhalifa@csumb.edu>
2017-10-16 05:50:18 +0000

    lab5 conig

--- dbConnection.php
+++ dbConnection.php
@@ -5,8 +5,18 @@ function dbConnection()
          $dbname='tcp';
          $username='root';
          $password="";
-         $dbConn= new PDO("mysql:host=$host;dbname=$dbname", $username,$password);
+         //
         $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $dbConn;
+        if(strpos($_SERVER['HTTP_HOST'],'herokuapp')!==false){
+            $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $host=$url["host"];
+            $dbname=substr($url["path"],1);
+            $username=$url["user"];
+            $password=$url["pass"];
+        }
+       $dbConn= new PDO("mysql:host=$host;dbname=$dbname", $username,$password);
+        
+        
 }    
 ?>
