01b681ead42958af744daf790570c93307703a37
Mohamad Khalifa <mkhalifa@csumb.edu>
2017-10-16 06:00:54 +0000

    final comit

--- dbConnection.php
+++ dbConnection.php
@@ -6,8 +6,7 @@ function dbConnection()
          $username='root';
          $password="";
          //
-        $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $dbConn;
+        
         if(strpos($_SERVER['HTTP_HOST'],'herokuapp')!==false){
             $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
             $host=$url["host"];
@@ -16,6 +15,9 @@ function dbConnection()
             $password=$url["pass"];
         }
        $dbConn= new PDO("mysql:host=$host;dbname=$dbname", $username,$password);
+       $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbConn;
+       
         
         
 }    
