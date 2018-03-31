fb613b2ffab5e8195afc6cb3229ccf0a6be95201
Mohamad Khalifa <mkhalifa@csumb.edu>
2018-03-31 18:41:58 +0000

    last change

--- workspace/index.php
+++ workspace/index.php
@@ -21,28 +21,20 @@
         
         <img id="SignUp" src="img/SignUpBtn.png" alt="Sign Up">
         
-        <div id="Navigation">
-            <div class="navItem"><span class="itemTitle">Login</span></div>
-            <div class="navItem"><span class="itemTitle">Matching</span></div>
-            <div class="navItem"><span class="itemTitle">Chat</span></div>
-            <div class="navItem"><span class="itemTitle">Location</span></div>
-        </div>
-        
         <div class="section" id="Logo">
-            <div class="container">
+            <div class="contain">
                 <div class="wrapper">
                   <div class="centered full">
-                      <img id="LogoPicture" src="img/LunchPalsLogo.png" alt="Logo">
+                      <img id="LogoPicture" src="img/Riri.jpg" alt="Logo">
                   </div>
                   <div class="centered">
-                    <h1>Meet New People And Explore New Places</h1>
+                    <h1>Welcome to Reham's Blog</h1>
                     <p>
-                        LunchPals is an easy and convenient way to connect you with other likeminded people and explore new and exciting places. 
-                        Focusing on students first we enable you to meet new friends when moving to new campuses and exploring your current campus.
-                        The first release of LunchPals will be on android, but don't worry: iOs is on it's way!
+                        I am a writer and copyeditor, and although I sometimes go off on long<br> rants with my copy, I am usually so efficient I wonder if I should add in a few exclamation<br>
+						points to make the content look longer.
+                        I am the founder of The Chama Karma Collective, a platform through which I curate and organize events where creatives can meet superstars like themselves, and a place for me to publicly publish social media rants that people care about.
                         <br/><br/>
-                        LunchPals also wants you to be able to find matches safely. 
-                        To allow this we are only matching at public places and allow you to rate each person you share a meal with.
+                      
                     </p>
                     </div>
                 </div>
@@ -50,168 +42,45 @@
         </div>
         
         <!-- ###################################### LOGIN ######################################-->
-        
-        <div class="section" id="Login">
+       
+      <style>#section{
+	 border: 5px solid grey;
+	  text-align:left;
+	  }#hey{float:left}
+	  #header{
+	  font-style: Snell Roundhand;
+	  color:yellow;
+	  }
+	  </style>
+		<div class="section" id="Matching">
             <div class="wrapper">
                 <div class="half centered centeredContainer">
                     <div class="centeredCell">
-                        <img class="product" src="img/LogoScreen.png">
+					<h1 id="header">WHATS ON YA MIND!</h1>
+					<p>
+                        <textarea class="product" id="section" rows="10" cols="10" wrap="hard">
+							</textarea>
+							<button  class="btn btn-success" onclick="randfunc()">POST</button>
+					</p>		
                     </div>
+					<div id="not">
+					</div>
                 </div>
             </div>
-            <div class="container">
-                <div class="wrapper">
-                    <div class="centeredContainer left half">
-                        <div class="centeredCell">
-                            <h1>Choose Your Way Of Signing In</h1>
-                            <p>
-                               With LunchPals you can choose how you wish to login!<br/>
-                               By allowing users to sign in with out facebook it makes it simpler sign up, 
-                               and allows users who don't have a facebook to join in the fun!
-                            </p>
-                        </div>
-                    </div>
-                    <div class="centeredContainer right half">
-                        <div class="centeredCell">
-                        <img class="product" src="img/LoginWithMail.png">
-                        </div>
-                    </div>
-                    <div class="clear"></div>
-                </div>
-                
-                <div class="wrapper">
-                    <div class="centeredContainer left half">
-                        <div class="centeredCell">
-                            <img class="product" src="img/LoginWithFacebook.png">
-                        </div>
-                    </div>
-                    <div class="centeredContainer right half">
-                        <div class="centeredCell">
-                            <h1>Sign In With Facebook</h1>
-                            <p>
-                               We also provide facebook sign in!<br/>
-                               You by using this sign in form you can add friends, which allows you to ask specific 
-                               people to join you! Using Facebook Signin allows you access to more features!
-                            </p>
-                        </div>
-                    </div>
-                    <div class="clear"></div>
-                </div>
-                
-            </div>
-        </div>
-        
-        <!-- ###################################### MATCHING ######################################-->
-        
-        <div class="section" id="Matching">
-            <div class="wrapper">
-                <div class="half centered centeredContainer">
-                    <div class="centeredCell">
-                        <img class="product" src="img/HomeScreen.png">
-                    </div>
-                </div>
-            </div>
-            
-            <div class="container">
-                
-                <div class="wrapper">
-                    <div class="centeredContainer left half">
-                        <div class="centeredCell">
-                            <h1>Apply Filters To Your Searches</h1>
-                            <p>
-                                To create the best possible experience, prior to the matching process you are able to enable some filter options.<br/>
-                                Burgers, Pizza or Seafood? You get to choose!<br/>
-                                Fancy, cheap or something in between? Choose your price range!<br/>
-                                Set even more filters like the preferred size of your group or the radius to search for matching pals.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="centeredContainer right half">
-                        <div class="centeredCell">
-                        <img class="product" src="img/Filters.png">
-                        </div>
-                    </div>
-                    <div class="clear"></div>
-                </div>
-                
-                <div class="wrapper">
-                    <div class="centeredContainer left half">
-                        <div class="centeredCell">
-                            <img class="product" src="img/MatchConfirmed.png">
-                        </div>
-                    </div>
-                    <div class="centeredContainer right half">
-                        <div class="centeredCell">
-                            <h1>Search For LunchPals</h1>
-                            <p>
-                                LunchPals is all about matching you with new people at new exciting restaurants.
-                                We will only match you based on your location, so you do not have to worry about driving to far.<br/>
-                                You do not own a car yourself? Just add the option to search for a pick up and one of your Pals will simply give you a ride to your match destination.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="clear"></div>
-                </div>
-                
-            </div>
-        </div>
-        
-        <!-- ###################################### CHAT ######################################-->
-        
-        <div class="section" id="Chat">
-            <div class="container">
-                <div class="wrapper">
-                    <div class="centeredContainer left half">
-                        <div class="centeredCell">
-                            <h1>Communicate With Your Match</h1>
-                            <p>
-                                We provide you with an interactive chat for you to connect to Pals you matched up with.<br/>
-                                It's a cool, easy and fun way to communicate before - and after - meeting up.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="centeredContainer right half">
-                        <div class="centeredCell">
-                            <img class="product" src="img/Chat.png" ><br>
-                        </div>
-                    </div>
-                    <div class="clear"></div>
-                </div>
-
-             </div>
-        </div>
-        
-        <!-- ###################################### LOCATION ######################################-->
-        
-        <div class="section" id="Location">
-            <div class="container">
-                <div class="wrapper">
-                    
-                    <div class="centeredContainer left half">
-                        <div class="centeredCell">
-                            <img class="product" src="img/Directions.png" >
-                        </div>
-                    </div>
-                    
-                    <div class="centeredContainer right half">
-                        <div class="centeredCell"> 
-                            <h1>Get Accurate Directions</h1>
-                            <p>
-                               Once you and your Pals decided on a time, you will be given directions to the
-                               location of the restaurant.<br>
-                               LunchPals is using the Google Maps API to provide you with accurate and fast navigation.
-                            </p>
-                        </div>
-                    </div>
-                    
-                    
-                </div>
-             </div>
-        
-        </div>
+			</div>
+			
+			
+			<script>
+			function randfunc(){
+			 
+			 var x = document.getElementById("section").value;
+			 alert(x);
+			 }
+			</script>
+			
         
         <footer class="section">
-            <div class="container">
+            <div class="contain">
     
                 <div class="wrapper">
                     <div class="centeredContainer left half">
@@ -235,14 +104,6 @@
                             </button>
                             </form>
                       </div>
-                    </div>
-                    <div class="centeredContainer right half">
-                        <div class="centeredCell">
-                            <div class="centered">
-                                <a href="#"><h1><span>&#8681;</span> Download LunchPals</h1></a>
-                                <a href="about.php"><p>About Us</p></a>
-                            </div>
-                        </div>
                     </div>
                         
                 </div>
